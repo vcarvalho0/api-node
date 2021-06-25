1f9f74ca775d7bba7ddd17626b081c9306ba1db0
vcarvalho0 <vitor1.santos@yahoo.com.br>
2021-06-25 16:46:08 -0300

    add model

---
+++ src/model/IUser.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+interface IUser extends Document {
+  discordId: number;
+  nickname: string;
+  description?: string;
+}
+
+const UserSchema = new Schema({
+  _id: {
+    type: Number,
+    required: true
+  },
+
+  nickname: {
+    type: String,
+    required: true
+  },
+});
+
+export default model<IUser>("User", UserSchema);
